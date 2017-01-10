37683052e71305e10d360ebc4a212e8570d6e20d
Tibor Udvari <tibor.udvari@icloud.com>
2017-01-10 20:07:15 +0100

    Removed lettering since now works with mouse n touch

--- sketch.js
+++ sketch.js
@@ -48,45 +48,46 @@ function setupValues(){
 }
 
 function setup(){
-		createCanvas(window.innerWidth, window.innerHeight);
-		
-		setupValues();
+	createCanvas(window.innerWidth, window.innerHeight);
 
-		setInterval(function(){
-			noobTimeline = (noobTimeline + 1) % divs;
+	setupValues();
 
-			for (var i = 0; i < keys.length; i++){
-				var beats = timelineData[i].beats;
-				if (beats.indexOf(noobTimeline) > -1) {
-					currentVidIdx = i;
-					var vid = videos[currentVidIdx];
-					var playing = vid.elt.currentTime > 0 && vid.elt.paused === false && vid.elt.ended === false;
-					if (vid.elt.paused) {
-						vid.play();
-					}
-					if ( !playing ){
-					}
-				} 
-			}
+	setInterval(function(){
+		noobTimeline = (noobTimeline + 1) % divs;
 
-			pVidIdx = currentVidIdx;
-		}, loopTime);
+		for (var i = 0; i < keys.length; i++){
+			var beats = timelineData[i].beats;
+			if (beats.indexOf(noobTimeline) > -1) {
+				currentVidIdx = i;
+				var vid = videos[currentVidIdx];
+				var playing = vid.elt.currentTime > 0 && vid.elt.paused === false && vid.elt.ended === false;
+				if (vid.elt.paused) {
+					started = true;
 
-		for (var i = 0; i < videos.length; i++){
-			timelineData.push( {"key": keys[i], "beats": []} );
-			keyToIdx[keys[i]] = i;
+					vid.play();
+				}
+				if ( !playing ){
+				}
+			} 
 		}
 
-		textAlign(RIGHT, CENTER);
-		textSize(10);
-		fill(255);
+		pVidIdx = currentVidIdx;
+	}, loopTime);
 
+	for (var i = 0; i < videos.length; i++){
+		timelineData.push( {"key": keys[i], "beats": []} );
+		keyToIdx[keys[i]] = i;
+	}
 
-		var el = document.getElementsByTagName("canvas")[0];
-el.addEventListener("touchstart", handleStart, false);
-el.addEventListener("touchend", handleEnd, false);
-el.addEventListener("touchcancel", handleCancel, false);
-el.addEventListener("touchmove", handleMove, false);
+	textAlign(RIGHT, CENTER);
+	textSize(10);
+	fill(255);
+
+	var el = document.getElementsByTagName("canvas")[0];
+	el.addEventListener("touchstart", handleStart, false);
+	el.addEventListener("touchend", handleEnd, false);
+	el.addEventListener("touchcancel", handleCancel, false);
+	el.addEventListener("touchmove", handleMove, false);
 }
 
 function showTitle(){
@@ -119,102 +120,98 @@ function draw() {
 		 image(videos[currentVidIdx], width / 2 - s / 2,  .1 * height, s, s); // draw the video frame to canvas
 		 pg.image(videos[currentVidIdx], 0, 0, s / window.devicePixelRatio, s / window.devicePixelRatio);
 		} else {
-		image(pg, width / 2 - s / 2,  .1 * height, s, s);
-	}
+			image(pg, width / 2 - s / 2,  .1 * height, s, s);
+		}
 
-	filter('GRAY');
-	fill(255);
-	push();
-
-	translate(width / 2 - s / 2,  .5 * height);
-	rect(0, 0, s, s );
-
-	line(noobTimeline * timelineIncrement, 0, noobTimeline * timelineIncrement, s);
-	stroke(0);
-
-	var delta = s / videos.length;
-	for (var i = 0; i < videos.length; i++) {
-		var y = (i + 1) * delta;
-		line(0, y, width, y);
+		filter('GRAY');
 		fill(255);
-		text(keys[i].toUpperCase(), -10, y - delta / 2);
+		push();
 
-		var beatsData = timelineData[i].beats;
-		beatsData.forEach(function(t) {
-			fill(0);
-			rect(t * timelineIncrement, i * delta, delta, delta);
-		});
+		translate(width / 2 - s / 2,  .5 * height);
+		rect(0, 0, s, s );
+
+		line(noobTimeline * timelineIncrement, 0, noobTimeline * timelineIncrement, s);
+		stroke(0);
+
+		var delta = s / videos.length;
+		for (var i = 0; i < videos.length; i++) {
+			var y = (i + 1) * delta;
+			line(0, y, width, y);
+			fill(255);
+			//text(keys[i].toUpperCase(), -10, y - delta / 2);
+
+			var beatsData = timelineData[i].beats;
+			beatsData.forEach(function(t) {
+				fill(0);
+				rect(t * timelineIncrement, i * delta, delta, delta);
+			});
+		}
+
+		pop();
 	}
 
-	pop();
-}
-
-function touchStarted(){
-	console.log("touch started");
-	handleInputAt(touches[0].x, touches[0].y);
-}
-
-function mousePressed(){
-	handleInputAt(mouseX, mouseY);
-}
-
-function handleInputAt(x, y) {
-	var h = y - height * .5;
-	var start = width / 2 - s / 2;
-	var delta = s / divs;
-	var timeIdx = 0;
-
-	if ( x >= start && x <= start + divs * delta){
-		timeIdx = Math.floor((x - start) / delta);
-	} else {
-		return;
+	function touchStarted(){
+		handleInputAt(touches[0].x, touches[0].y);
 	}
 
-	var delta = s / videos.length;
-	var idx = Math.floor(h / delta);
-	idx = constrain(idx, 0, videos.length - 1);
-	handleInputForIdx(idx, timeIdx);
-}
-
-function handleInputForIdx(idx, timeIdx){
-	started = true;
-
-	var beats = timelineData[idx].beats;
-	var beatIdx = beats.indexOf(timeIdx);
-	if (beatIdx > -1) {
-		beats.splice(beatIdx, 1);
-	} else {
-		beats.push(timeIdx);
-		videos[idx].play();
-	} 
-}
-
-function keyPressed() {
-	var k = key.toLowerCase();
-	if (keys.indexOf(k) > -1) {
-		var idx = keyToIdx[k];
-		handleInputForIdx(idx, noobTimeline);
+	function mousePressed(){
+		handleInputAt(mouseX, mouseY);
 	}
-}
 
-function handleStart(e){
-	e.preventDefault();
-}
+	function handleInputAt(x, y) {
+		var h = y - height * .5;
+		var start = width / 2 - s / 2;
+		var delta = s / divs;
+		var timeIdx = 0;
 
-function handleEnd(e){
-	e.preventDefault();
-}
+		if ( x >= start && x <= start + divs * delta){
+			timeIdx = Math.floor((x - start) / delta);
+		} else {
+			return;
+		}
 
-function handleCancel(e){
-	e.preventDefault();
-}
+		var delta = s / videos.length;
+		var idx = Math.floor(h / delta);
+		idx = constrain(idx, 0, videos.length - 1);
+		handleInputForIdx(idx, timeIdx);
+	}
 
-function handleMove(e){
-	e.preventDefault();
-}
+	function handleInputForIdx(idx, timeIdx){
+		var beats = timelineData[idx].beats;
+		var beatIdx = beats.indexOf(timeIdx);
+		if (beatIdx > -1) {
+			beats.splice(beatIdx, 1);
+		} else {
+			beats.push(timeIdx);
+			videos[idx].play();
+		} 
+	}
 
-function windowResized() {
-	console.log("window resized");
-  resizeCanvas(windowWidth, windowHeight);
-  setupValues();
-}
+	function keyPressed() {
+		var k = key.toLowerCase();
+		if (keys.indexOf(k) > -1) {
+			var idx = keyToIdx[k];
+			handleInputForIdx(idx, noobTimeline);
+		}
+	}
+
+	function handleStart(e){
+		e.preventDefault();
+	}
+
+	function handleEnd(e){
+		e.preventDefault();
+	}
+
+	function handleCancel(e){
+		e.preventDefault();
+	}
+
+	function handleMove(e){
+		e.preventDefault();
+	}
+
+	function windowResized() {
+		resizeCanvas(windowWidth, windowHeight);
+		setupValues();
+	}
